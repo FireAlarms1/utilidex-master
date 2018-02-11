fa426bdc17ec23a514158f05389bc29ba49f0df2
FireAlarms1 <30513844+FireAlarms1@users.noreply.github.com>
2018-02-10 23:15:53 -0800

    Add files via upload

---
+++ lag.js
@@ -0,0 +1,12 @@
+
+const Discord = require("discord.js");
+
+module.exports.run = (bot, message, args) => {
+  if (command === "lag") {
+  message.channel.send("lag my dude!")
+};
+};
+
+module.exports.help = {
+  name: "lag"
+}
